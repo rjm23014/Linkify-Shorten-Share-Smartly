484064fde6d73e8134641c4b98392d71ebb9c22a
rjm23014 <csds23014@glbitm.ac.in>
2025-07-20 10:23:21 +0530

    Created redirect route for short URL access

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
 from shortener import shorten_url
 from qr_generator import generate_qr
+from db import init_db, get_long_url, increment_visit, get_visits
 import os
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'static'
+init_db()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -12,8 +13,15 @@ def index():
         long_url = request.form['long_url']
         short_url = shorten_url(long_url)
         qr_code_path = generate_qr(short_url)
-        return render_template('result.html', short_url=short_url, qr_image=qr_code_path)
+        short_code = short_url.split('/')[-1]
+        visits = get_visits(short_code)
+        return render_template('result.html', short_url=short_url, qr_image=qr_code_path, visits=visits)
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/<short_code>')
+def redirect_to_long(short_code):
+    long_url = get_long_url(short_code)
+    if long_url:
+        increment_visit(short_code)
+        return redirect(long_url)
+    return "<h1>URL not found</h1>", 404
